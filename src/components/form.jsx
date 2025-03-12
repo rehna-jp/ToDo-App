37f75cd6496920fd1f5c13a9098abb23cd59765d
rehna-jp <jeremyrehna05@gmail.com>
2025-03-12 11:01:26 +0000

    made some changes

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -17,7 +17,7 @@ function Form(props) {
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
+          What  to be done?
         </label>
       </h2>
       <input
